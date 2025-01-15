cf5b32e09296492978a9b8b81228486c9a648d40
1-鱼皮 <592789970@qq.com>
2025-01-15 22:35:18 +0800

    第 14 期 - 项目上线 - 前后端 1. 后端修改 pom.xml 文件，支持打包 2. 前端区分开发环境和生产环境

--- yu-picture-frontend/src/utils/pictureEditWebSocket.ts
+++ yu-picture-frontend/src/utils/pictureEditWebSocket.ts
@@ -13,7 +13,10 @@ export default class PictureEditWebSocket {
    * 初始化 WebSocket 连接
    */
   connect() {
-    const url = `ws://localhost:8123/api/ws/picture/edit?pictureId=${this.pictureId}`
+    const DEV_BASE_URL = "ws://localhost:8123";
+    // 线上地址
+    // const PROD_BASE_URL = "ws://81.69.229.63";
+    const url = `${DEV_BASE_URL}/api/ws/picture/edit?pictureId=${this.pictureId}`
     this.socket = new WebSocket(url)
 
     // 设置携带 cookie
